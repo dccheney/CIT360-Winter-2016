4729d16e95c43529a127e4c32a6689a5ccacc801
dccheney <che09012@byui.edu>
2016-02-23 11:40:14 -0700

    My UML added

---
+++ Sandbox/David/Collections/Map.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+public class Tree{
+
+    public static void main(String args[]){
+        HashMap<String, String> hmap = new HashMap<String, String>();
+        hmap.put("fName", "David");
+        hmap.put("lName", "Cheney");
+        hmap.put("mInit", null);
+        hmap.put("College", "BYU-I");
+        hmap.put("Major", "Computer Information Technology");
+        System.out.println(hmap);
+        System.out.println(hmap);
+    }
+}
