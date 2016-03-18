6f22de2e372d055519040bb5751b641993971e97
dccheney <che09012@byui.edu>
2016-03-18 12:01:44 -0600

    Update to apc

--- Sandbox/David/ACP/Application.java
+++ Sandbox/David/ACP/Application.java
@@ -14,12 +14,18 @@ public class Application {
 
         String command;
         Boolean running = true;
+        controller.mapCommand("tuneup", new Tuneup());
+        controller.mapCommand("advanced tuneup", new AdvancedTuneup());
+        controller.mapCommand("screen replacement", new ScreenReplacement());
+        controller.mapCommand("reinstall os", new ReinstallOS());
+        controller.mapCommand("250gb ssd upgrade", new SSDUpgrade());
+
         while (running == true) {
             System.out.println();
             System.out.println("Which service would you like to know more about (Tuneup, Advanced Tuneup,");
             System.out.println("Screen Replacement, Reinstall OS, 250GB SSD upgrade, or enter 'quit' to exit)?");
 
-            command = in.nextLine();
+            command = in.nextLine().toLowerCase();
             if (command.equals("quit"))
                 running = false;
 
--- Sandbox/David/ACP/ApplicationController.java
+++ Sandbox/David/ACP/ApplicationController.java
@@ -6,15 +6,10 @@ import java.util.HashMap;
  * Created by David on 3/15/2016.
  */
 public class ApplicationController {
-    public static HashMap<String, Handler> handlerHashMap = new HashMap<String, Handler>();
+    private static HashMap<String, Handler> handlerHashMap = new HashMap<String, Handler>();
 
-    public ApplicationController() {
-        handlerHashMap.put("Tuneup", new Tuneup());
-        handlerHashMap.put("Advanced Tuneup", new AdvancedTuneup());
-        handlerHashMap.put("Screen Replacement", new ScreenReplacement());
-        handlerHashMap.put("Reinstall OS", new ReinstallOS());
-        handlerHashMap.put("250GB SSD upgrade", new SSDUpgrade());
-    }
+    public void mapCommand(String aCommand, Handler acHandler) {handlerHashMap.put(aCommand, acHandler);}
+
     public static void handleUserCommand(String command, HashMap parameters) {
         Handler handler = handlerHashMap.get(command);
         if(handler != null) {
